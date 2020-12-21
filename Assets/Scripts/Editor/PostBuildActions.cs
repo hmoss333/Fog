99f28a858f97f09db637c7e30e607a5d4b6d0d73
hmoss333 <hdamoss@gmail.com>
2020-12-21 11:22:24 -0500

    Update 12/21/2020
    -Pushing latest changes

---
+++ Assets/Scripts/Editor/PostBuildActions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class PostBuildActions
+{
+    [PostProcessBuild]
+    public static void OnPostProcessBuild(BuildTarget target, string targetPath)
+    {
+        string path = Path.Combine(targetPath, "Build/UnityLoader.js");
+        string text = File.ReadAllText(path);
+
+        text = text.Replace("UnityLoader.SystemInfo.mobile", "false");
+        File.WriteAllText(path, text);
+    }
+}
